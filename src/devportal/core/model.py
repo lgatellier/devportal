f5ef5b8c8ba64ca90823cc73a56119b3c529d406
Léo GATELLIER <26511053+lgatellier@users.noreply.github.com>
2024-09-06 00:20:45 +0200

    feat: add app creation feature

--- src/devportal/core/model.py
+++ src/devportal/core/model.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from sqlmodel import Field, SQLModel, create_engine, Session
 
 engine = create_engine(
@@ -5,15 +6,25 @@ engine = create_engine(
 )
 
 
-class Application(SQLModel, table=True):
+class Type(Enum):
+    batch = "batch"
+    library = "library"
+    service = "service"
+
+
+class ApplicationBase(SQLModel):
+    code: str = Field(min_length=1)
+    name: str = Field(min_length=1)
+    description: str = Field(min_length=1)
+    type: Type = Field(default=Type.service, index=True)
+
+
+class Application(ApplicationBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    code: str
-    name: str
-    description: str
 
 
 def get_session():
     return Session(engine)
 
 
-__all__ = ["get_session", "Application"]
+__all__ = ["engine", "get_session", "Application"]
